2657edbbdc885a44427586a7d5dc76bc46b7a43d
muhsin61 <muhsin_61_67@hotmail.com>
2020-08-11 12:47:51 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,
+    </p>
+    <div id="nav">
+      <router-link to="/">Podomoro</router-link> |
+      <router-link to="/shortBreak">Short Break</router-link> |
+      <router-link to="/longBreak">Long Break</router-link>
+    </div>
+    <div class="buttons">
+      <button>Start</button>
+      <button>Stop</button>
+      <button>Reset</button>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
